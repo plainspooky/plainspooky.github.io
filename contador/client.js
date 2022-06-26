66cd154ccc97adc2ee5940f09edeee539a5a541c
Giovanni Nunes <giovanni.nunes@gmail.com>
2022-06-26 14:19:01 -0300

    Acrescenta mais uma data à contagem refressiva e muda o visual

--- contador/client.js
+++ contador/client.js
@@ -2,52 +2,61 @@ const MINUTES = 60;
 const HOURS = MINUTES * 60;
 const DAYS = HOURS * 24;
 
-const countUntil = new Date("Dec 31 2022 23:59:59 GMT-0300");
+const electionDay = "Oct 2 2022 08:00:00 GMT-0300";
+const lastDayOfHim = "Dec 31 2022 23:59:59 GMT-0300";
+const useAlternateDate = window.location.href.search("year");
 
-const daysId = document.getElementById('countdown-days');
-const hoursId = document.getElementById('countdown-hours');
-const minutesId = document.getElementById('countdown-minutes');
-const secondsId = document.getElementById('countdown-seconds');
+const getEndDate = () => {
+  return new Date(useAlternateDate >= 0 ? lastDayOfHim : electionDay);
+};
 
 const updateCountDown = () => {
-  let days, hours, minutes, seconds = 0;
+  let days,
+    hours,
+    minutes,
+    seconds = 0;
   const justNow = new Date();
 
   seconds = (countUntil - justNow) / 1000;
 
-  if (seconds>0) {
+  if (seconds > 0) {
     days = Math.floor(seconds / DAYS);
-    seconds %= (days * DAYS);
+    seconds %= days * DAYS;
     hours = Math.floor(seconds / HOURS);
-    
-    if (hours !== 0) seconds %= (hours * HOURS);
-    
+
+    if (hours !== 0) seconds %= hours * HOURS;
+
     minutes = Math.floor(seconds / MINUTES);
-    
+
     if (minutes !== 0) seconds = Math.floor(seconds % (minutes * MINUTES));
 
     updateValues(days, hours, minutes, seconds);
   }
 
   setTimeout(updateCountDown, 1000);
-}
+};
 
 const updateValues = (days, hours, minutes, seconds) => {
   updateElement(daysId, days, "dia");
   updateElement(hoursId, hours, "hora");
   updateElement(minutesId, minutes, "minuto");
   updateElement(secondsId, seconds, "segundo");
-}
+};
 
 const updateElement = (element, value, label) => {
   const labelElement = element.parentElement.getElementsByTagName("small");
   element.textContent = value;
   labelElement[0].textContent = pluralize(value, label);
-}
+};
 
 const pluralize = (value, word) => {
-  return word + (value != 1 ? "s"  : "");
-}
+  return word + (value != 1 ? "s" : "");
+};
 
-if (daysId && hoursId && minutesId && secondsId) updateCountDown()
+const countUntil = getEndDate();
+const daysId = document.getElementById("countdown-days");
+const hoursId = document.getElementById("countdown-hours");
+const minutesId = document.getElementById("countdown-minutes");
+const secondsId = document.getElementById("countdown-seconds");
 
+if (daysId && hoursId && minutesId && secondsId) updateCountDown();
